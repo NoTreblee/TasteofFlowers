51cc980b275a5114bd3b568e51cda7ffb7fcafa4
NoTreblee <fiercestword@gmail.com>
2022-05-30 23:38:21 +0200

    added logo animation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('.navbar')
-    const allNavItems = document.querySelectorAll('.nav-link')
-    const navList = document.querySelector('.navbar-collapse')
-
-    function addShadow() {
-        if (window.scrollY >= 300) {
-            nav.classList.add('shadow-bg')
-        } else {
-            nav.classList.remove('shadow-bg')
-        }
+  const nav = document.querySelector('.navbar');
+  const allNavItems = document.querySelectorAll('.nav-link');
+  const navList = document.querySelector('.navbar-collapse');
+  const logo = document.querySelector('.logo');
+  function addShadow() {
+    if (window.scrollY >= 300) {
+      nav.classList.add('shadow-bg');
+      logo.classList.add('none');
+    } else {
+      nav.classList.remove('shadow-bg');
+      logo.classList.remove('none');
     }
+  }
 
-    allNavItems.forEach(item => item.addEventListener('click', () => navList.classList.remove('show')))
+  allNavItems.forEach((item) =>
+    item.addEventListener('click', () => navList.classList.remove('show'))
+  );
 
-    window.addEventListener('scroll', addShadow)
-
-})
+  window.addEventListener('scroll', addShadow);
+});
